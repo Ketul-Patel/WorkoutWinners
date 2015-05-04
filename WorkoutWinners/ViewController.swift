a8c7f814c539c9fe3820d45679f4d55ba9a9f49d
Ketul Patel <kjpatel2010@gmail.com>
2015-05-04 13:30:34 -0700

    initial commit

--- WorkoutWinners/ViewController.swift
+++ WorkoutWinners/ViewController.swift
@@ -7,19 +7,74 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var stepCount: UILabel!
+    @IBOutlet weak var totalScore: UILabel!
+    @IBOutlet weak var flightCount: UILabel!
+    @IBOutlet weak var multiplier: UILabel!
+    
+    var steps = 0
+    var total: Double = 0
+    var mult = 1.0
+    var flights = 0
+    var distance = 0
+    var timeStart: NSDate? = nil
+    
+    @IBAction func resetButtonPressed(sender: UIButton) {
+        steps = 0
+        total = 0
+        mult = 1
+        flights = 0
+        distance = 0
+        timeStart = nil
+        self.updateUI()
+    }
+    @IBAction func startButtonPressed(sender: UIButton) {
+        println("pressed")
+        self.timeStart = NSDate()
+        println("start = \(self.timeStart)")
+        // start takes in a date and a closure
+        pedometer.startPedometerUpdatesFromDate(NSDate(), withHandler: { data, error in
+            println("Update \(data.numberOfSteps)")
+            println("Distance = \(data.distance)")
+            // dispatch async
+            dispatch_async(dispatch_get_main_queue()) {
+                self.steps = Int(data.numberOfSteps)
+                self.flights = Int(data.floorsAscended) + Int(data.floorsDescended)
+                self.distance = Int(data.distance)
+                self.updateScore()
+                self.updateUI()
+            }
+        })
+    }
+    
+    let pedometer: CMPedometer = CMPedometer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        stepCount.text = steps.description
+        totalScore.text = total.description
+        flightCount.text = flights.description
+        multiplier.text = mult.description + "x"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func updateScore() {
+        var intervalCalculator = NSDate.timeIntervalSinceDate(timeStart!)
+        var interval = intervalCalculator(NSDate())
+        interval *= -1
+        println("Interval = \(interval)")
+        mult = Double(distance) / interval
+        println("Mult = \(mult)")
+        total = Double(steps + flights*10)*mult
     }
-
-
+    func updateUI() {
+        multiplier.text = String(format: "%.2f", mult) + "x"
+        totalScore.text = String(format: "%.2f", total)
+        stepCount.text = String(stringInterpolationSegment: steps)
+        flightCount.text = flights.description
+    }
+    
 }
 
